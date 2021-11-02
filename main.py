5ebdd6072917643e4e295465c0ddaeba1ffde178
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2021-11-02 19:16:52 -0400

    end game statistics

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ import game_traces.selectors as gs
 
 
 def main():
+    # Real run:
     # board_parameters = o.input_game_settings()
+
+    # Dev run:
     board_parameters = m.input_mock_fast_game_settings()
 
     game = Game(board_parameters=board_parameters, recommend=True)
 
     game.play(algo=Game.MINIMAX, player_x=Game.AI, player_o=Game.AI,
               board_parameters=board_parameters, file=gs.select_game_traces_file(board_parameters))
-    # game.play(algo=Game.MINIMAX, player_x=Game.AI, player_o=Game.AI,
-    #           board_parameters=board_parameters)
 
 
 if __name__ == "__main__":
